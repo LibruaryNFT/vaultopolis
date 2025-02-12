87bd9fb88bfe8946fc092297285420683cff3e4a
LibruaryNFT <libruarynft@gmail.com>
2025-02-12 10:35:09 -0500

    Force https

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { UserProvider } from "./components/UserContext";
 import Header from "./components/Header";
@@ -11,6 +11,16 @@ import Footer from "./components/Footer";
 import Layout from "./components/Layout";
 import Sell from "./components/Sell";
 import Swap from "./components/Swap";
+
+// Enforce HTTPS only in production
+function enforceHTTPS() {
+  if (
+    window.location.protocol !== "https:" &&
+    window.location.hostname !== "localhost"
+  ) {
+    window.location.href = `https://${window.location.hostname}${window.location.pathname}${window.location.search}`;
+  }
+}
 
 // Create the router
 const router = createBrowserRouter(
@@ -76,6 +86,10 @@ const router = createBrowserRouter(
 );
 
 function App() {
+  useEffect(() => {
+    enforceHTTPS();
+  }, []);
+
   return (
     <UserProvider>
       <div className="w-full min-h-screen bg-gray-600">
