179aff55746194b72d4b81d2d8f6ea140888a555
LibruaryNFT <libruarynft@gmail.com>
2025-03-19 18:23:32 -0500

    Changed banner colour

--- src/components/AnnouncementBanner.js
+++ src/components/AnnouncementBanner.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 function AnnouncementBanner() {
   return (
-    <div className="bg-indigo-800 text-white text-center py-2">
+    <div className="bg-purple-600 text-white text-center py-2">
       <p className="text-sm sm:text-base">
         <strong>Introducing TSHOT:</strong> a new token minted 1:1 from Top Shot
         Common/Fandom Moments.{" "}
