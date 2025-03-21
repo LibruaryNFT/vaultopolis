02506510f9bf5468182a934fcbcc1a9ac909b451
LibruaryNFT <libruarynft@gmail.com>
2025-03-21 13:19:21 -0500

    Button text fix

--- src/pages/Transfer.js
+++ src/pages/Transfer.js
@@ -252,7 +252,7 @@ const Transfer = () => {
           onClick={handleTransfer}
           disabled={transferDisabled}
           className={`
-            w-full p-4 text-lg rounded-lg font-bold text-white
+            w-full p-4 text-lg rounded-lg font-bold
             mt-2 transition-colors
             ${
               transferDisabled
