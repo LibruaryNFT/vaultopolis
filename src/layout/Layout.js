68e610a6c073a438e96a306db0e4e69478c09829
Libruary <libruarynft@gmail.com>
2025-05-01 09:29:23 -0500

    Removed banner

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,27 +1,16 @@
 // src/layout/Layout.jsx
 import React from "react";
-import { useLocation } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
-import AnnouncementBanner from "../components/AnnouncementBanner";
 
-const Layout = ({ children }) => {
-  const { pathname } = useLocation();
+const Layout = ({ children }) => (
+  <div className="flex flex-col min-h-screen relative">
+    <Header />
 
-  // Show the banner only on "/" and "/swap"
-  const showBanner = pathname === "/" || pathname === "/swap";
+    <main className="flex-1 px-0 sm:px-2 md:px-4">{children}</main>
 
-  return (
-    <div className="flex flex-col min-h-screen relative">
-      <Header />
-
-      {showBanner && <AnnouncementBanner />}
-
-      <main className="flex-1 px-0 sm:px-2 md:px-4">{children}</main>
-
-      <Footer />
-    </div>
-  );
-};
+    <Footer />
+  </div>
+);
 
 export default Layout;
